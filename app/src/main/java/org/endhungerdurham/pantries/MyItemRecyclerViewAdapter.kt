9e3ac37964778b607509de27f04ceefa32051727
Rob Gries <robert.w.gries@gmail.com>
2018-11-05 23:02:01 -0500

    Remove unneceessary comments

--- app/src/main/java/org/endhungerdurham/pantries/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/org/endhungerdurham/pantries/MyItemRecyclerViewAdapter.kt
@@ -9,11 +9,6 @@ import org.endhungerdurham.pantries.ItemsFragment.OnListFragmentInteractionListe
 
 import kotlinx.android.synthetic.main.fragment_item.view.*
 
-/**
- * [RecyclerView.Adapter] that can display a [DummyItem] and makes a call to the
- * specified [OnListFragmentInteractionListener].
- * TODO: Replace the implementation with code for your data type.
- */
 class MyItemRecyclerViewAdapter(
         private val mValues: List<Pantry>,
         private val mListener: OnListFragmentInteractionListener?)
