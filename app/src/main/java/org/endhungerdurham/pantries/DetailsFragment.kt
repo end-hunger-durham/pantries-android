f95d7212116a12fb9763a4110b02c2b3c520e365
Rob Gries <robert.w.gries@gmail.com>
2018-11-08 11:19:47 -0500

    Use constraint layouts and clean up layouts

--- app/src/main/java/org/endhungerdurham/pantries/DetailsFragment.kt
+++ app/src/main/java/org/endhungerdurham/pantries/DetailsFragment.kt
@@ -14,10 +14,23 @@ class DetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_details, container, false)
-        val pantry = arguments?.getParcelable<Pantry>(ARG_PANTRY)
-        val detailsText = view.findViewById<TextView>(R.id.address)
-        detailsText.append("\n" + pantry?.address + pantry?.city)
+        fillDetails(view, arguments?.getParcelable<Pantry>(ARG_PANTRY))
+
         return view
+    }
+
+    private fun fillDetails(view: View, pantry: Pantry?) {
+        val addressText = view.findViewById<TextView>(R.id.address)
+        addressText.append("\n" + pantry?.address + pantry?.city)
+
+        val avaialabilityText = view.findViewById<TextView>(R.id.avaialability)
+        avaialabilityText.append("\n" + pantry?.days + pantry?.hours)
+
+        val qualsText = view.findViewById<TextView>(R.id.qualifications)
+        qualsText.append("\n" + pantry?.prereq)
+
+        val infoText = view.findViewById<TextView>(R.id.info)
+        infoText.append("\n" + pantry?.info)
     }
 
     companion object {
